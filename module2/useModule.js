b3466e61967927468716edf7e3ebd48f94b6d559
GoEun <w2219@e-mirim.hs.kr>
2024-03-28 15:36:54 +0900

    module실습

--- module2/useModule.js
+++ module2/useModule.js
@@ -2,9 +2,11 @@ const {text_odd, text_even} = require('./moduleText');
 
 function checkStringOddOrEven(str){
     if(str.length % 2){
-        return text_odd;
+        return text_even;
     }
-    return text_even;
+    return text_odd;
 }
-console.log("3: "+checkStringOddOrEven(3));
-console.log("4: "+checkStringOddOrEven(4));
+console.log("3: "+checkStringOddOrEven("안녕"));
+console.log("4: "+checkStringOddOrEven("dkdsd"));
+
+module.exports = [checkStringOddOrEven, text_even, text_odd];
